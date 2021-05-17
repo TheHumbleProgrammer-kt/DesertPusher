8f50053481e2b6a4b4ab2a29e29c25cb410e7f4a
TheHumbleProgrammer-kt <dmandasher@gmail.com>
2021-05-17 02:20:21 -0400

    message

--- DessertClickerLogs/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ DessertClickerLogs/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -18,6 +18,7 @@ package com.example.android.dessertclicker
 
 import android.content.ActivityNotFoundException
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -25,13 +26,20 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.Lifecycle
 import com.example.android.dessertclicker.databinding.ActivityMainBinding
 import timber.log.Timber
+
+const val KEY_REVENUE = "revenue_key"
+const val KEY_DESSERT_SOLD = "dessert_sold_key"
+const val KEY_TIMER_SECONDS = "timer_seconds_key"
 
 class MainActivity : AppCompatActivity() {
 
     private var revenue = 0
     private var dessertsSold = 0
+    private lateinit var dessertTimer: DessertTimer
 
     // Contains all the views
     private lateinit var binding: ActivityMainBinding
@@ -66,6 +74,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        fun reinstateInstanceState() {
+            if (savedInstanceState != null) {
+                revenue = savedInstanceState.getInt(KEY_REVENUE, 0)
+                dessertsSold = savedInstanceState.getInt(KEY_DESSERT_SOLD, 0)
+                revenue = savedInstanceState.getInt(KEY_REVENUE, 0)
+                dessertTimer.secondsCount = savedInstanceState.getInt(KEY_TIMER_SECONDS, 0)
+                showCurrentDessert()
+            }
+        }
+
         Timber.i("onCreate called")
 
         // Use Data Binding to get reference to the views
@@ -75,12 +93,26 @@ class MainActivity : AppCompatActivity() {
             onDessertClicked()
         }
 
+        dessertTimer = DessertTimer(this.lifecycle)
+
+        reinstateInstanceState()
+
         // Set the TextViews to the right values
         binding.revenue = revenue
         binding.amountSold = dessertsSold
 
         // Make sure the correct dessert is showing
         binding.dessertButton.setImageResource(currentDessert.imageId)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt(KEY_REVENUE, revenue)
+        outState.putInt(KEY_DESSERT_SOLD, dessertsSold)
+        outState.putInt(KEY_TIMER_SECONDS, dessertTimer.secondsCount)
+
+        Timber.i("onSaveInstanceState Called")
     }
 
     /**
@@ -155,11 +187,14 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
 
         Timber.i("onStart called")
+
     }
 
     override fun onResume() {
         super.onResume()
         Timber.i("onResume Called")
+
+
     }
 
     override fun onPause() {
@@ -170,6 +205,8 @@ class MainActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         Timber.i("onStop Called")
+
+
     }
 
     override fun onDestroy() {
@@ -180,5 +217,6 @@ class MainActivity : AppCompatActivity() {
     override fun onRestart() {
         super.onRestart()
         Timber.i("onRestart Called")
+
     }
 }
